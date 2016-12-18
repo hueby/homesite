b5b8e127bdd47554a12e22bba8aedee72b1165ec
Dennis <dennis@detux.local>
2016-12-18 11:30:03 +0100

    initial commit

---
+++ app/controllers/projects.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+/*
+ *  Thoughts on project format
+ *
+ *  var project = {
+ *    name: "Project 101",
+ *    desc: "Some things about the project"
+ *    stars: { github stars some funtion },
+ *    link: "github-link",
+ *    pics: [],
+ *    dateCreated: "timestamp",
+ *    documentationLink: "link"
+ *  }
+ *
+ *  var projects = [project, project, ...];
+ *
+ *  stored in mongo/parse?
+ *  what is ember data? later use ember data
+ *  for now parse
+ * */
+
+export default Ember.Controller.extend({
+});
